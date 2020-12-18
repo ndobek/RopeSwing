273660c9c280ddcca803a26b835e30951d26a95c
Fano <n.j.dobek@gmail.com>
2020-12-17 17:16:56 -0700

    Before moving away from rigidbody

--- Assets/Scripts/HumanController.cs
+++ Assets/Scripts/HumanController.cs
@@ -12,9 +12,12 @@ public class HumanController : MonoBehaviour, IController
     private float cameraYaw;
 
     public float moveSpeed;
+    public float maxAirSpeed;
     public float sprintSpeed;
-    //public float stepHeight;
-    //public float maxSlope;
+    public float maxStepHeight;
+
+    [SerializeField]
+    private float stepCheckerResolution;
 
     private bool sprinting;
     public bool snappyAirControl = false;
@@ -34,24 +37,53 @@ public class HumanController : MonoBehaviour, IController
         {
             Vector3 v = Quaternion.Euler(0, lookTransform.rotation.eulerAngles.y, 0) * input;
             v *= sprinting ? sprintSpeed : moveSpeed;
+            //CheckStep(v, stepRayCastDistance);
             rigidBody.velocity = new Vector3(v.x, newVelocity.y, v.z);
         }
         else
-        {
-            Vector3 v = Quaternion.Euler(0, lookTransform.rotation.eulerAngles.y, 0) * input;
-            v *= airControl;
+        {          
+            Vector3 localdv = input *= airControl;
+            Vector3 localv = lookTransform.InverseTransformVector(rigidBody.velocity);
 
-            if(snappyAirControl) rigidBody.MovePosition(movementTransform.position + v);
-            else rigidBody.velocity += v;
+            if (snappyAirControl)
+            {
+                Vector3 globaldv = Quaternion.Euler(0, lookTransform.rotation.eulerAngles.y, 0) * localdv;
+                rigidBody.MovePosition(movementTransform.position + globaldv);
+            }
+            else
+            {
+                Vector3 v = new Vector3(localv.z, localv.y, localv.z);
+                if (Mathf.Abs(localv.x + localdv.x) < maxAirSpeed) localv.x =  Mathf.Clamp(localv.x + localdv.x, -maxAirSpeed, maxAirSpeed);
+
+                //if (localv.y + localdv.y < maxAirSpeed) localv.y += localdv.y;
+                if (Mathf.Abs(localv.z + localdv.z) < maxAirSpeed) localv.z =  Mathf.Clamp(localv.z + localdv.z, -maxAirSpeed, maxAirSpeed);
+
+                rigidBody.velocity = lookTransform.TransformVector(localv);
+            }
         }
     }
 
     public void Look(Vector3 input)
     {
-        cameraPitch += input.x;
+        cameraPitch = Mathf.Clamp(cameraPitch + input.x, -90, 90);
         cameraYaw += input.y;
         lookTransform.rotation = Quaternion.Euler(new Vector3(cameraPitch, cameraYaw, 0));
     }
+
+    public void CheckStep(Vector3 point)
+    {
+        Vector3 pointToCheck = groundChecker.position;
+        for(float offset = 0; offset < maxStepHeight; offset += maxStepHeight/stepCheckerResolution)
+        {
+            
+        }
+
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red; 
+    }
+
 
     public void Jump()
     {
